20ec30cd368118d4b4f60b892662a458a3da0b4b
00013096 <shamshod918@gmail.com>
2024-10-31 20:38:26 +0500

    Controllers

---
+++ DSCC_CW1_MVC/Controllers/DepartmentController.cs
@@ -0,0 +1,170 @@
+﻿using DSCC_CW1_Api.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DSCC_CW1_MVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly string BaseUrl = "http://localhost:7110/";
+
+        // GET: DepartmentController
+        public async Task<ActionResult> Index()
+        {
+            List<DepartmentDto> departments = new List<DepartmentDto>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/department/GetAll");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    departments = JsonConvert.DeserializeObject<List<DepartmentDto>>(responseContent);
+                }
+            }
+            return View(departments);
+        }
+
+        // GET: DepartmentController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            DepartmentDto department = new DepartmentDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/department/GetById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    department = JsonConvert.DeserializeObject<DepartmentDto>(responseContent);
+                }
+            }
+            return View(department);
+        }
+
+        // GET: DepartmentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DepartmentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(DepartmentCreateDto department)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(department);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("api/department/Create", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
+        }
+
+        // GET: DepartmentController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            DepartmentDto department = new DepartmentDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/department/GetById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    department = JsonConvert.DeserializeObject<DepartmentDto>(responseContent);
+                }
+            }
+            return View(department);
+        }
+
+        // POST: DepartmentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, DepartmentCreateDto departmentUpdateDto)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(departmentUpdateDto);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PutAsync($"api/department/Update/{id}", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(departmentUpdateDto);
+        }
+
+        // GET: DepartmentController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            DepartmentDto department = new DepartmentDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/department/GetById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    department = JsonConvert.DeserializeObject<DepartmentDto>(responseContent);
+                }
+            }
+            return View(department);
+        }
+
+        // POST: DepartmentController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.DeleteAsync($"api/department/Delete/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ DSCC_CW1_MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,170 @@
+﻿using DSCC_CW1_Api.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DSCC_CW1_MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly string BaseUrl = "http://localhost:7110/";
+
+        // GET: EmployeeController
+        public async Task<ActionResult> Index()
+        {
+            List<EmployeeDto> employees = new List<EmployeeDto>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/employee/GetAll");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    employees = JsonConvert.DeserializeObject<List<EmployeeDto>>(responseContent);
+                }
+            }
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+            EmployeeDto employee = new EmployeeDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/employee/GetById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    employee = JsonConvert.DeserializeObject<EmployeeDto>(responseContent);
+                }
+            }
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(EmployeeCreateDto employee)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(employee);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("api/employee/Create", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Edit/5
+        public async Task<ActionResult> Edit(int id)
+        {
+            EmployeeDto employee = new EmployeeDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/employee/GetById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    employee = JsonConvert.DeserializeObject<EmployeeDto>(responseContent);
+                }
+            }
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, EmployeeCreateDto employeeUpdateDto)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var jsonContent = JsonConvert.SerializeObject(employeeUpdateDto);
+                var contentString = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+                var response = await client.PutAsync($"api/employee/Update/{id}", contentString);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(employeeUpdateDto);
+        }
+
+        // GET: EmployeeController/Delete/5
+        public async Task<ActionResult> Delete(int id)
+        {
+            EmployeeDto employee = new EmployeeDto();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync($"api/employee/GetById/{id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    employee = JsonConvert.DeserializeObject<EmployeeDto>(responseContent);
+                }
+            }
+            return View(employee);
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.DeleteAsync($"api/employee/Delete/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View();
+        }
+    }
+}
